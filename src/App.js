7448c3b7e77170709030cd0e213855e75c618338
fran-exfer <fran.exfer@gmail.com>
2021-05-03 15:01:30 +0200

    Add colors, set up mock json-server.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3004/clients')
+      .then((data) => data.json())
+      .then((json) => setClients(json));
+  }, []);
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>{client.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
