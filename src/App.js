8b45f65b17072c423345ff27319beea949051c6b
fran-exfer <fran.exfer@gmail.com>
2021-05-03 17:37:56 +0200

    Basic login functionality.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import AuthPanel from './components/authentication/AuthPanel';
 function App() {
   const [authed, setAuthed] = useState(false);
 
+  const handleAuthSubmit = (event, username, password) => {
+    event.preventDefault();
+
+    fetch('http://localhost:3004/auth')
+      .then((data) => data.json())
+      .then((json) => {
+        if (username === json.username && password === json.password) {
+          setAuthed(true);
+        } else {
+          console.log('NOOOPE');
+        }
+      });
+  };
+
   if (!authed) {
     return (
       <main>
-        <AuthPanel />
+        <AuthPanel onSubmit={handleAuthSubmit} />
       </main>
     );
   }
 
-  return <main></main>;
+  return (
+    <main>
+      <h1>Logged in!</h1>
+    </main>
+  );
 }
 
 export default App;
--- src/components/authentication/AuthPanel.js
+++ src/components/authentication/AuthPanel.js
@@ -1,19 +1,34 @@
-function AuthPanel() {
+import { useRef } from 'react';
+
+import styles from './AuthPanel.module.scss';
+
+function AuthPanel({ onSubmit }) {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
   return (
-    <section>
+    <section className={styles.authpanel}>
       <div className="panel">
         <h1>Please log in</h1>
-        <form>
+        <form
+          onSubmit={(event) =>
+            onSubmit(
+              event,
+              usernameRef.current.value,
+              passwordRef.current.value
+            )
+          }
+        >
           <div className="control">
             <label>Admin username:</label>
-            <input type="text"></input>
+            <input type="text" ref={usernameRef} />
           </div>
           <div className="control">
             <label>Admin password:</label>
-            <input type="password"></input>
+            <input type="password" ref={passwordRef} />
           </div>
           <div className="flex justify-center">
-            <button class="btn btn--primary">Log in</button>
+            <button className="btn btn--primary">Log in</button>
           </div>
         </form>
       </div>
